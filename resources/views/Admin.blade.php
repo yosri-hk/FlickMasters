dc51beb19d5ee9cf39ddc4a495bfb0a8b1cccb0c
rimbsaad <rim.bensaad@esprit.tn>
2023-10-02 23:51:50 +0200

    crud-products

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -189,10 +189,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/dist/img/am3-new.png" class="img-circle elevation-2" alt="User Image">
+          <img src="/dist/img/rim_img.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="/admin" class="d-block">Amine Barguellil</a>
+          <a href="/admin" class="d-block">Rim Ben Saad</a>
         </div>
       </div>
 
@@ -252,7 +252,7 @@
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tree"></i>
+            <i class="nav-icon fas fa-shopping-bag"></i>              
               <p>
                 Produit
                 <i class="fas fa-angle-left right"></i>
