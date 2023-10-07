1bd9a38eb0fb7c39cdc423c24c3cceb86cb5c24a
Amine <amine.barguellil@esprit.tn>
2023-10-07 11:09:56 +0100

    Integ-Front-Back🚀

--- resources/views/articles/liste.blade.php
+++ resources/views/articles/liste.blade.php
@@ -400,7 +400,7 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script>
+    <!-- <script>
         const articleMore = document.getElementById("article-more");
         const readMoreLink = document.getElementById("read-more-link");
 
@@ -414,7 +414,7 @@
                 readMoreLink.textContent = "Voir plus";
             }
         });
-    </script>
+    </script> -->
         <!-- liste article -->
         </div>
         <!-- /.row -->
