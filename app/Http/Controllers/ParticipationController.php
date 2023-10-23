e1137922e87ac2dfe90e6ddd5eedb5542fc28f43
Ones Diouani <odiouani@winter-mushroom.com>
2023-10-23 17:21:26 +0200

    crud operation for participation entity

--- app/Http/Controllers/ParticipationController.php
+++ app/Http/Controllers/ParticipationController.php
@@ -22,17 +22,16 @@ class ParticipationController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'nom_participant' => 'required',
-            'cin_participant' => 'required|unique:participations,cin_participant',
-            'adresse_email' => 'required|email',
-            'date_de_participation' => 'required|date',
-            'event_id' => 'required',
+        $data = $request->validate([
+            "nom_participant" => "required|string|between:5,20",
+            "cin_participant" => "required|string|between:5,100",
+            "adresse_email" => "required|email",
+            "telephone" => "required|string",
+            "event_id" => "required|numeric",  
         ]);
-
-        Participation::create($request->all());
-
-        return redirect()->route('/');
+        //Participation::create($request->all());
+        $participation=Participation::create($data);
+        return redirect('/');
 
     }
 
